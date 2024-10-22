d80c0d78908e4a622a66611d42f3ebb1748e9c41
Richard <richard@mail.kway.com.tw>
2024-10-22 15:24:21 +0800

    加入專案檔案。

---
+++ JsonDemo2_1734/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+
+namespace JsonDemo2_1734
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var guaranteeData = new GuaranteeData 
+            {
+                InvestorAccount = "DavidWang1204",
+                GuaranteeDate = DateTime.Now,
+                GuaranteeSerialNumber = "G12345",
+                CommissionDate = DateTime.Now,
+                CommissionSerialNumber = "Y12345",
+                ReportedGuaranteeAmount = 9000,
+                GuaranteeMaintainRate = 80,
+                ExceededGuaranteeRate = 90,
+                FunctionalCode = "Hello",
+                TransactionStatus = "Success",
+                RepaymentStatus = "Success"
+            };
+            string jsonData = JsonConvert.SerializeObject(guaranteeData, Formatting.Indented);
+            Console.WriteLine(jsonData);
+        }
+    }
+    public class GuaranteeData
+    {
+        public string InvestorAccount { get; set; }
+        public DateTime GuaranteeDate { get; set; }
+        public string GuaranteeSerialNumber { get; set; }
+        public DateTime CommissionDate { get; set; }
+        public string CommissionSerialNumber { get; set; }  
+        public string TransactionStatus { get; set; }
+        public string RepaymentStatus { get; set; }
+        public decimal ReportedGuaranteeAmount { get; set; }
+        public decimal GuaranteeMaintainRate { get; set; }
+        public decimal ExceededGuaranteeRate { get; set; }
+        public string FunctionalCode { get; set; }
+       
+    }
+}
