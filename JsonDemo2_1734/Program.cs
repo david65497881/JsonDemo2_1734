90cd3cd9e58cef8422f10cec3d562a5d20167b09
Richard <richard@mail.kway.com.tw>
2024-10-23 10:46:36 +0800

    修改程式碼

--- JsonDemo2_1734/Program.cs
+++ JsonDemo2_1734/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System;
 using Newtonsoft.Json;
+using System.Transactions;
 
 namespace JsonDemo2_1734
 {
@@ -8,34 +9,45 @@ namespace JsonDemo2_1734
     {
         static void Main(string[] args)
         {
-            //宣告gunarnteeData裡的資料
-            var guaranteeData = new GuaranteeData 
+            
+            GuaranteeWrapper guaranteeWrapper = new GuaranteeWrapper
             {
-                InvestorAccount = "DavidWang1204",
-                GuaranteeDate = DateTime.Now,
-                GuaranteeSerialNumber = "G12345",
-                CommissionDate = DateTime.Now,
-                CommissionSerialNumber = "Y12345",
-                ReportedGuaranteeAmount = 9000,
-                GuaranteeMaintainRate = 80,
-                ExceededGuaranteeRate = 90,
-                FunctionalCode = "Hello",
-                TransactionStatus = "Success",
-                RepaymentStatus = "Success"
+                SearchCriteria = new SearchCriteria
+                {
+                    CollateralDate = DateTime.Now,
+                    InvestorAccount = "DavidWang1204",
+                    TransactionStatus = "Success",
+                    RepaymentStatus = "Success"
+                },
+                GuaranteeData = new GuaranteeData
+                {
+                    InvestorAccount = "DavidWang1204",
+                    GuaranteeDate = DateTime.Now,
+                    GuaranteeSerialNumber = "G12345",
+                    CommissionDate = DateTime.Now,
+                    CommissionSerialNumber = "Y12345",
+                    ReportedGuaranteeAmount = 9000,
+                    GuaranteeMaintainRate = 80,
+                    ExceededGuaranteeRate = 90,
+                    FunctionalCode = "Hello",
+                    TransactionStatus = "Success",
+                    RepaymentStatus = "Success"
+                }
             };
+
 
             //.SerializeObject用於將guaranteeData的資料轉換成JSON字串
             //Formatting.Indented用於美化排版,默認情況下是沒有縮排的   
-            string jsonData = JsonConvert.SerializeObject(guaranteeData, Formatting.Indented);
+            string jsonData = JsonConvert.SerializeObject(guaranteeWrapper, Formatting.Indented);
             Console.WriteLine(jsonData);
 
-            //第一段@"guaranteeData3.json"為檔案存入的路徑，檔案會被存入當專案執行的跟目錄下，也可以指定路徑寫成@"C:\data\guaranteeData3.json"
+            //第一段@"guaranteeWrapper.json"為檔案存入的路徑，檔案會被存入當專案執行的跟目錄下，也可以指定路徑寫成@"C:\data\guaranteeWrapper.json"
             //第二段jsonData為要存入的資料
             //@ 符號表示這是一個逐字字串，可以直接使用反斜線而不用加上跳脫字符
             //System.IO.File.WriteAllText用來將字串內容寫入檔案的靜態方法，如果檔案已存在，會直接覆蓋，如果檔案不存在，會自動創建
-            System.IO.File.WriteAllText(@"guaranteeData.json", jsonData);
+            System.IO.File.WriteAllText(@"guaranteeWrapper.json", jsonData);
 
-            Console.WriteLine("JSON data has been saved to guaranteeData.json");
+            Console.WriteLine("JSON data has been saved to guaranteeWrapper.json");
 
             Console.ReadLine();
 
@@ -44,7 +56,7 @@ namespace JsonDemo2_1734
 
             //定義完整檔案路徑，Path.Combine是C#裡合併路徑的靜態方法，作用是將多個字串組合成一個合法的檔案或資料夾路徑，並且會自動處理路徑之間的分隔符號
             //例如，當你手動拼接路徑時，如果不小心忘記加上分隔符號或重複加上，就可能產生不正確的路徑。而 Path.Combine 則會自動處理這些情況，確保結果是正確的
-            string filePath = Path.Combine(baseDirectory,@"guaranteeData.json");
+            string filePath = Path.Combine(baseDirectory, @"guaranteeWrapper.json");
 
             //如果檔案存在，反序列化並輸出，如果不存在，顯示檔案不存在
             if (File.Exists(filePath))
@@ -53,19 +65,19 @@ namespace JsonDemo2_1734
                 string jsonData1 = File.ReadAllText(filePath);
 
                 //DeserializeObject<GuaranteeData>(jsonData1)將 JSON 字串轉換為 C# 中的物件
-                GuaranteeData guaranteeData1 = JsonConvert.DeserializeObject<GuaranteeData>(jsonData1);
+                GuaranteeWrapper guaranteeWrapper1 = JsonConvert.DeserializeObject<GuaranteeWrapper>(jsonData1);
 
-                Console.WriteLine("InvestorAccount :" + guaranteeData1.InvestorAccount);
-                Console.WriteLine("GuaranteeDate :" + guaranteeData1.GuaranteeDate);
-                Console.WriteLine("GuaranteeSerialNumber :" + guaranteeData1.GuaranteeSerialNumber);
-                Console.WriteLine("CommissionDate :" + guaranteeData1.CommissionDate);
-                Console.WriteLine("CommissionSerialNumber :" + guaranteeData1.CommissionSerialNumber);
-                Console.WriteLine("ReportedGuaranteeAmount :" + guaranteeData1.ReportedGuaranteeAmount);
-                Console.WriteLine("GuaranteeMaintainRate :" + guaranteeData1.GuaranteeMaintainRate);
-                Console.WriteLine("ExceededGuaranteeRate :" + guaranteeData1.ExceededGuaranteeRate);
-                Console.WriteLine("FunctionalCode :" + guaranteeData1.FunctionalCode);
-                Console.WriteLine("TransactionStatus :" + guaranteeData1.TransactionStatus);
-                Console.WriteLine("RepaymentStatus :" + guaranteeData1.RepaymentStatus);
+                Console.WriteLine("InvestorAccount :" + guaranteeWrapper1.GuaranteeData.InvestorAccount);
+                Console.WriteLine("GuaranteeDate :" + guaranteeWrapper1.GuaranteeData.GuaranteeDate);
+                Console.WriteLine("GuaranteeSerialNumber :" + guaranteeWrapper1.GuaranteeData.GuaranteeSerialNumber);
+                Console.WriteLine("CommissionDate :" + guaranteeWrapper1.GuaranteeData.CommissionDate);
+                Console.WriteLine("CommissionSerialNumber :" + guaranteeWrapper1.GuaranteeData.CommissionSerialNumber);
+                Console.WriteLine("ReportedGuaranteeAmount :" + guaranteeWrapper1.GuaranteeData.ReportedGuaranteeAmount);
+                Console.WriteLine("GuaranteeMaintainRate :" + guaranteeWrapper1.GuaranteeData.GuaranteeMaintainRate);
+                Console.WriteLine("ExceededGuaranteeRate :" + guaranteeWrapper1.GuaranteeData.ExceededGuaranteeRate);
+                Console.WriteLine("FunctionalCode :" + guaranteeWrapper1.GuaranteeData.FunctionalCode);
+                Console.WriteLine("TransactionStatus :" + guaranteeWrapper1.GuaranteeData.TransactionStatus);
+                Console.WriteLine("RepaymentStatus :" + guaranteeWrapper1.GuaranteeData.RepaymentStatus);
             }
             else 
             {
@@ -75,6 +87,19 @@ namespace JsonDemo2_1734
 
 
     }
+
+    public class GuaranteeWrapper 
+    {
+        /// <summary>
+        /// 擔保資料
+        /// </summary>
+        public GuaranteeData GuaranteeData { get; set; }
+        /// <summary>
+        /// 搜尋資料
+        /// </summary>
+        public SearchCriteria SearchCriteria { get; set; }
+    }
+
     public class GuaranteeData
     {
         /// <summary>
@@ -97,14 +122,7 @@ namespace JsonDemo2_1734
         /// 委託序號
         /// </summary>
         public string CommissionSerialNumber { get; set; }  
-        /// <summary>
-        /// 成交狀態
-        /// </summary>
-        public string TransactionStatus { get; set; }
-        /// <summary>
-        /// 還劵狀態
-        /// </summary>
-        public string RepaymentStatus { get; set; }
+        
         /// <summary>
         /// 已申報擔保總經額
         /// </summary>
@@ -121,6 +139,36 @@ namespace JsonDemo2_1734
         /// 功能碼
         /// </summary>
         public string FunctionalCode { get; set; }
-       
+        /// <summary>
+        /// 成交狀態
+        /// </summary>
+        public string TransactionStatus { get; set; }
+        /// <summary>
+        /// 還劵狀態
+        /// </summary>
+        public string RepaymentStatus { get; set; }
+
+
+    }
+
+    public class SearchCriteria 
+    {
+        /// <summary>
+        /// 擔保品日期
+        /// </summary>
+        public DateTime CollateralDate { get; set; }
+        /// <summary>
+        /// 投資人帳號
+        /// </summary>
+        public string InvestorAccount {get; set; }
+        /// <summary>
+        /// 成交狀態
+        /// </summary>
+        public string TransactionStatus { get; set; }
+        /// <summary>
+        /// 還劵狀態
+        /// </summary>
+        public string RepaymentStatus { get; set; }
+
     }
 }
